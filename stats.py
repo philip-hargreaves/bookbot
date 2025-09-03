dfd385d262942e9fe1012a2ec2c0b235cda82aa7
philip-hargreaves <philiphargreaves@live.co.uk>
2025-09-03 17:21:59 +0100

    Add word and letter count functions

--- stats.py
+++ stats.py
@@ -5,5 +5,18 @@ def count_letters(book_text):
     letter_freq = {}
     for word in book_text.split():
         for letter in word.lower():
-            letter_freq[letter] = letter_freq.get(letter, 0) + 1
+            if letter.isalpha():
+                letter_freq[letter] = letter_freq.get(letter, 0) + 1
     return letter_freq
+
+def sort_dict(letter_freq):
+    dict_list = []
+    for key, value in letter_freq.items():
+        formatted_dict = {"char": key, "num": value}
+        dict_list.append(formatted_dict)
+    
+    def sort_on(letter_freq):
+        return letter_freq["num"]
+
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
