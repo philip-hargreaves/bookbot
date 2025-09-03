38d9a8e45155a5b2372267c66d07b2766e9ff094
philip-hargreaves <philiphargreaves@live.co.uk>
2025-09-03 17:40:43 +0100

    Add command line argument functionality

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import count_words, count_letters, sort_dict
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as file:
@@ -7,7 +8,11 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text("./books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_text = get_book_text(sys.argv[1])
     word_count = count_words(book_text)
 
     letter_freq = count_letters(book_text)
@@ -23,5 +28,5 @@ def main():
             print(f"{letter["char"]}: {letter["num"]}")
     print("============= END ===============")
 
-    
+
 main()
